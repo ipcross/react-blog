1dec20c007ad24eeed4ce190ba5b7ca93ac539df
cross <cross@vgg.ru>
2017-08-03 09:31:25 +0300

    Refactoring.

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -1,22 +1,23 @@
 import nightmare from 'nightmare';
+import { APP_ROOT } from 'constants/API';
 
 describe('When visit the homepage', () => {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
   it('welcomes the user', async () => {
-    let page = nightmare().goto('http://192.168.23.148:3000');
+    let page = nightmare().goto(APP_ROOT);
     let text = await page.evaluate(() => document.body.textContent).end();
     expect(text).toContain('ReactBlog');
   });
 
   it('goto about', async () => {
-    let page = nightmare().goto('http://192.168.23.148:3000').click('a[href$="/about"]');
+    let page = nightmare().goto(APP_ROOT).click('a[href$="/about"]');
     let text = await page.evaluate(() => document.body.textContent).end();
     expect(text).toContain('About page');
   });
 
   it('goto contacts', async () => {
-    let page = nightmare().goto('http://192.168.23.148:3000').click('a[href$="/contacts"]');
+    let page = nightmare().goto(APP_ROOT).click('a[href$="/contacts"]');
     let text = await page.evaluate(() => document.body.textContent).end();
     expect(text).toContain('Message');
   });
